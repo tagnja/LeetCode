190375b457717bc1bbb0178b61873da9b7f6db20
taogen <taogenjia@gmail.com>
2021-03-22 12:19:15 +0800

    [feat] Finished the problem [1] Two Sum

---
+++ leetcode-java/src/main/java/leetcode/editor/en/TwoSum.java
@@ -0,0 +1,78 @@
+//Given an array of integers nums and an integer target, return indices of the t
+//wo numbers such that they add up to target.
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice.
+//
+// You can return the answer in any order.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+//
+//
+// Example 3:
+//
+//
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+//
+//
+//
+// Constraints:
+//
+//
+// 2 <= nums.length <= 103
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+//
+// Related Topics Array Hash Table
+// 👍 19877 👎 700
+
+
+package leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TwoSum{
+  public static void main(String[] args) {
+       Solution solution = new TwoSum().new Solution();
+       int nums[] = {3, 2, 4};
+       int target = 6;
+      int[] result = solution.twoSum(nums, target);
+      for (int i = 0; i < result.length; i++) {
+          System.out.println(result[i]);
+      }
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return null;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
